86cbb4648c5da755b1ed0a00616f796a20b98e1e
Vincent Lapie <vincent.lapie@laposte.net>
2016-06-12 00:20:26 +0200

    Correction display grey

--- imports/modal.js
+++ imports/modal.js
@@ -6,60 +6,63 @@ import './modal.html';
 Template.MyModal.onCreated(function(){
   // TODO : get params
 });
-//
-// Template.MyMap.onCreated(function() {
-//
-//     GoogleMaps.ready('myMap', function(map) {
-//    // Add a marker to the map once it's ready
-//    var marker = new google.maps.Marker({
-//      position: map.options.center,
-//      map: map.instance
-//    });
-//  });
-// });
-//
-// Template.MyMap.helpers({
-//
-//   myMapOptions: function() {
-//     // Make sure the maps API has loaded
-//     if (GoogleMaps.loaded()) {
-//       // Map initialization options
-//       return {
-//         center: new google.maps.LatLng(-37.8136, 144.9631),
-//         zoom: 8
-//       };
-//     }
-//   }
-// });
-// Template.MyModal.onRendered(function() {
-//    GoogleMaps.load();
-// });
-
-
-
 
 Template.MyMap.onCreated(function() {
-    GoogleMaps.load();
+
     GoogleMaps.ready('myMap', function(map) {
-        // Add a marker to the map once it's ready
-        var marker = new google.maps.Marker({
-            position: map.options.center,
-            map: map.instance
-        });
-    });
+   // Add a marker to the map once it's ready
+   var marker = new google.maps.Marker({
+     position: map.options.center,
+     map: map.instance
+   });
+ });
 });
 
 Template.MyMap.helpers({
-    myMapOptions: function() {
-        var data = Template.currentData(self.view);
-        if (!data) return;
-        // Make sure the maps API has loaded
-        if (data && GoogleMaps.loaded()) {
-            // Map initialization options
-            return {
-                center: new google.maps.LatLng(-37.8136, 144.9631),
-                zoom: 17
-            };
-        }
+
+  myMapOptions: function() {
+    // Make sure the maps API has loaded
+    if (GoogleMaps.loaded()) {
+      // Map initialization options
+      return {
+        center: new google.maps.LatLng(-37.8136, 144.9631),
+        zoom: 8
+      };
     }
+
+  }
 });
+Template.MyMap.onRendered(function() {
+   GoogleMaps.load();
+});
+
+
+
+
+
+
+// Template.MyMap.onCreated(function() {
+//     GoogleMaps.load();
+//     GoogleMaps.ready('myMap', function(map) {
+//         // Add a marker to the map once it's ready
+//         var marker = new google.maps.Marker({
+//             position: map.options.center,
+//             map: map.instance
+//         });
+//     });
+// });
+//
+// Template.MyMap.helpers({
+//     myMapOptions: function() {
+//         var data = Template.currentData(self.view);
+//         if (!data) return;
+//         // Make sure the maps API has loaded
+//         if (data && GoogleMaps.loaded()) {
+//             // Map initialization options
+//             return {
+//                 center: new google.maps.LatLng(-37.8136, 144.9631),
+//                 zoom: 17
+//             };
+//         }
+//     }
+// });
